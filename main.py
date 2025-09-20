7b7222cb2cb2fb0e4b5d6c14f11b5ef54ce44b1b
Mahla Nejati <mnej691@UoA.auckland.ac.nz>
2025-09-20 21:46:46 +1200

    new version

--- main.py
+++ main.py
@@ -4,20 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
+import glob
 
 # Choose to use a config and initialize the detector
 config = 'configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py'
 # Setup a checkpoint file to load
 checkpoint = 'tomato/laboro_tomato_48ep.pth'
 # initialize the detector
-print("there")
 model = init_detector(config, checkpoint, device='cuda:0')
-print("here")
+
 # Use the detector to do inference
-img = 'tomato/IMG_1003.jpg'
-result = inference_detector(model, img)
+#img = 'tomato/Outdoor/tomato1.jpg'
+for img in glob.glob('/tomato/Outdoor/*'):
+    result = inference_detector(model, img)
 
 # Let's plot the result
-show_result_pyplot(model, img, result, score_thr=0.3)
+    show_result_pyplot(model, img, result, score_thr=0.3)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
